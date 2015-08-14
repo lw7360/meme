bcf3c1e4bf1a1c4c42c3d1e84f668667d0ed41ca
Larry Wu <lw7360@gmail.com>
2015-08-13 22:53:09 -0700

    Basic project outline

---
+++ setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='meme',
+
+    version='1.0.0',
+
+    description='A Meme CLI tool',
+    long_description=long_description,
+
+    url='https://github.com/lw7360/meme',
+
+    author='Larry Wu',
+    author_email='lw7360@gmail.com',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Graphics',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='dank memes',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    install_requires=['Pillow'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'meme=meme:main',
+        ],
+    },
+)
